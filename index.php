58380c29052ecab52e4f3605b69c3d8c1bd5f4f5
Derek Robson <derekinsantanin@gmail.com>
2026-05-25 13:57:06 -0300

    vai tomando

--- index.php
+++ index.php
@@ -61,6 +61,8 @@
 </div>
     <button onclick="limpar()" class="btn-limpar">Limpar</button>
 </div>
+<h1>vai tomando
+</h1>
 <script>    
     function mascaraCEP(input) {
         // Remove tudo que não for número
